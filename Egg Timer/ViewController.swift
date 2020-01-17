38726683af8e4856c244866a58700ee314be5d77
Annekatrin Dunkel <annekatrin.dunkel@gmx.de>
2020-01-17 16:20:58 +0100

    Initial Commit

---
+++ Egg Timer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Egg Timer
+//
+//  Created by Annekatrin Dunkel on 1/17/20.
+//  Copyright © 2020 Annekatrin Dunkel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
